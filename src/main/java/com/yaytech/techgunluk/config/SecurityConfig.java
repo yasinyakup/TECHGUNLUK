3db74284c54485c053baf6c3f39f8e9876931033
yasinyakup <Yasin@19841231>
2020-11-28 23:13:54 +0300

    Home page also Authenticated

--- src/main/java/com/yaytech/techgunluk/config/SecurityConfig.java
+++ src/main/java/com/yaytech/techgunluk/config/SecurityConfig.java
@@ -31,7 +31,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/auth/**").permitAll()
+                .antMatchers("/api/auth/**", "/home/**").permitAll()
                 .anyRequest()
                 .authenticated();
                 
