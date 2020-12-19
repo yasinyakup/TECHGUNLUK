dc78b8808a7399eb2238c36e2ab830bfdc5e10a2
yasin yakup <yasinyakup@gmail.com>
2020-12-19 18:06:39 +0300

    two dto classes added

---
+++ src/main/java/com/yaytech/techgunluk/dto/AuthenticationResponse.java
@@ -0,0 +1,13 @@
+package com.yaytech.techgunluk.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+    private String authenticationToken;
+    private String loginName;
+}
